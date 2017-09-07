f009da94fe28b60a4bce6687c65990597c857971
Ladar Levison <ladar@lavabit.com>
2017-09-07 13:22:38 -0500

    Added Cypherpunks to corpus. Fork mounting, and detect whether sudo is needed.

--- _messages.mount.sh
+++ _messages.mount.sh
@@ -1,56 +1,64 @@
 #!/bin/bash
 
+SUDO=""
 LINK=`readlink -f $0`
 BASE=`dirname $LINK`
+
+if [ `id -u` -ne 0 ]; then
+	SUDO="sudo"
+fi
 
 USERID=`id --user`
 GROUPID=`id --group`
 
 cd $BASE
 
-sudo archivemount ceas.1.tar.bz2 $BASE/ceas/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.2.tar.bz2 $BASE/ceas/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.3.tar.bz2 $BASE/ceas/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.4.tar.bz2 $BASE/ceas/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.5.tar.bz2 $BASE/ceas/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.6.tar.bz2 $BASE/ceas/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.7.tar.bz2 $BASE/ceas/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount ceas.8.tar.bz2 $BASE/ceas/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount ceas.1.tar.bz2 $BASE/ceas/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.2.tar.bz2 $BASE/ceas/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.3.tar.bz2 $BASE/ceas/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.4.tar.bz2 $BASE/ceas/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.5.tar.bz2 $BASE/ceas/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.6.tar.bz2 $BASE/ceas/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.7.tar.bz2 $BASE/ceas/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount ceas.8.tar.bz2 $BASE/ceas/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
-sudo archivemount corpus.1.tar.bz2 $BASE/corpus/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount corpus.1.tar.bz2 $BASE/corpus/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
-sudo archivemount enron.1.tar.bz2 $BASE/enron/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.2.tar.bz2 $BASE/enron/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.3.tar.bz2 $BASE/enron/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.4.tar.bz2 $BASE/enron/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.5.tar.bz2 $BASE/enron/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.6.tar.bz2 $BASE/enron/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.7.tar.bz2 $BASE/enron/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.8.tar.bz2 $BASE/enron/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.9.tar.bz2 $BASE/enron/_9 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.10.tar.bz2 $BASE/enron/_10 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount enron.11.tar.bz2 $BASE/enron/_11 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount cypher.1.tar.bz2 $BASE/cypher/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
-sudo archivemount openssl.1.tar.bz2 $BASE/openssl/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount openssl.2.tar.bz2 $BASE/openssl/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount openssl.3.tar.bz2 $BASE/openssl/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount openssl.4.tar.bz2 $BASE/openssl/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount openssl.5.tar.bz2 $BASE/openssl/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount enron.1.tar.bz2 $BASE/enron/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.2.tar.bz2 $BASE/enron/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.3.tar.bz2 $BASE/enron/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.4.tar.bz2 $BASE/enron/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.5.tar.bz2 $BASE/enron/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.6.tar.bz2 $BASE/enron/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.7.tar.bz2 $BASE/enron/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.8.tar.bz2 $BASE/enron/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.9.tar.bz2 $BASE/enron/_9 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.10.tar.bz2 $BASE/enron/_10 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount enron.11.tar.bz2 $BASE/enron/_11 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
-sudo archivemount trec.1.tar.bz2 $BASE/trec/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.2.tar.bz2 $BASE/trec/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.3.tar.bz2 $BASE/trec/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.4.tar.bz2 $BASE/trec/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.5.tar.bz2 $BASE/trec/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.6.tar.bz2 $BASE/trec/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.7.tar.bz2 $BASE/trec/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.8.tar.bz2 $BASE/trec/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.9.tar.bz2 $BASE/trec/_9 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.10.tar.bz2 $BASE/trec/_10 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.11.tar.bz2 $BASE/trec/_11 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.12.tar.bz2 $BASE/trec/_12 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
-sudo archivemount trec.13.tar.bz2 $BASE/trec/_13 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount openssl.1.tar.bz2 $BASE/openssl/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount openssl.2.tar.bz2 $BASE/openssl/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount openssl.3.tar.bz2 $BASE/openssl/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount openssl.4.tar.bz2 $BASE/openssl/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount openssl.5.tar.bz2 $BASE/openssl/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
-sudo archivemount xml.1.tar.bz2 $BASE/xml/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID
+($SUDO archivemount trec.1.tar.bz2 $BASE/trec/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.2.tar.bz2 $BASE/trec/_2 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.3.tar.bz2 $BASE/trec/_3 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.4.tar.bz2 $BASE/trec/_4 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.5.tar.bz2 $BASE/trec/_5 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.6.tar.bz2 $BASE/trec/_6 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.7.tar.bz2 $BASE/trec/_7 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.8.tar.bz2 $BASE/trec/_8 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.9.tar.bz2 $BASE/trec/_9 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.10.tar.bz2 $BASE/trec/_10 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.11.tar.bz2 $BASE/trec/_11 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.12.tar.bz2 $BASE/trec/_12 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+($SUDO archivemount trec.13.tar.bz2 $BASE/trec/_13 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
 
+($SUDO archivemount xml.1.tar.bz2 $BASE/xml/_1 -o readonly -o allow_other -o nonempty -o uid=$USERID -o gid=$GROUPID) &
+
+wait
 
--- _messages.unmount.sh
+++ _messages.unmount.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 
+SUDO=""
 LINK=`readlink -f $0`
 BASE=`dirname $LINK`
 
-USERID=`id --user`
-GROUPID=`id --group`
+if [ `id -u` -ne 0 ]; then
+	SUDO="sudo"
+fi
 
 cd $BASE
 
-sudo umount $BASE/ceas/_1
-sudo umount $BASE/ceas/_2
-sudo umount $BASE/ceas/_3
-sudo umount $BASE/ceas/_4
-sudo umount $BASE/ceas/_5
-sudo umount $BASE/ceas/_6
-sudo umount $BASE/ceas/_7
-sudo umount $BASE/ceas/_8
+$SUDO umount $BASE/ceas/_1
+$SUDO umount $BASE/ceas/_2
+$SUDO umount $BASE/ceas/_3
+$SUDO umount $BASE/ceas/_4
+$SUDO umount $BASE/ceas/_5
+$SUDO umount $BASE/ceas/_6
+$SUDO umount $BASE/ceas/_7
+$SUDO umount $BASE/ceas/_8
 
-sudo umount $BASE/corpus/_1
+$SUDO umount $BASE/corpus/_1
 
-sudo umount $BASE/enron/_1
-sudo umount $BASE/enron/_2
-sudo umount $BASE/enron/_3
-sudo umount $BASE/enron/_4
-sudo umount $BASE/enron/_5
-sudo umount $BASE/enron/_6
-sudo umount $BASE/enron/_7
-sudo umount $BASE/enron/_8
-sudo umount $BASE/enron/_9
-sudo umount $BASE/enron/_10
-sudo umount $BASE/enron/_11
+$SUDO umount $BASE/cypher/_1
 
-sudo umount $BASE/openssl/_1
-sudo umount $BASE/openssl/_2
-sudo umount $BASE/openssl/_3
-sudo umount $BASE/openssl/_4
-sudo umount $BASE/openssl/_5
+$SUDO umount $BASE/enron/_1
+$SUDO umount $BASE/enron/_2
+$SUDO umount $BASE/enron/_3
+$SUDO umount $BASE/enron/_4
+$SUDO umount $BASE/enron/_5
+$SUDO umount $BASE/enron/_6
+$SUDO umount $BASE/enron/_7
+$SUDO umount $BASE/enron/_8
+$SUDO umount $BASE/enron/_9
+$SUDO umount $BASE/enron/_10
+$SUDO umount $BASE/enron/_11
 
-sudo umount $BASE/trec/_1
-sudo umount $BASE/trec/_2
-sudo umount $BASE/trec/_3
-sudo umount $BASE/trec/_4
-sudo umount $BASE/trec/_5
-sudo umount $BASE/trec/_6
-sudo umount $BASE/trec/_7
-sudo umount $BASE/trec/_8
-sudo umount $BASE/trec/_9
-sudo umount $BASE/trec/_10
-sudo umount $BASE/trec/_11
-sudo umount $BASE/trec/_12
-sudo umount $BASE/trec/_13
+$SUDO umount $BASE/openssl/_1
+$SUDO umount $BASE/openssl/_2
+$SUDO umount $BASE/openssl/_3
+$SUDO umount $BASE/openssl/_4
+$SUDO umount $BASE/openssl/_5
 
-sudo umount $BASE/xml/_1
+$SUDO umount $BASE/trec/_1
+$SUDO umount $BASE/trec/_2
+$SUDO umount $BASE/trec/_3
+$SUDO umount $BASE/trec/_4
+$SUDO umount $BASE/trec/_5
+$SUDO umount $BASE/trec/_6
+$SUDO umount $BASE/trec/_7
+$SUDO umount $BASE/trec/_8
+$SUDO umount $BASE/trec/_9
+$SUDO umount $BASE/trec/_10
+$SUDO umount $BASE/trec/_11
+$SUDO umount $BASE/trec/_12
+$SUDO umount $BASE/trec/_13
+
+$SUDO umount $BASE/xml/_1
